5d66b18c8af938b000f179a21cc5330bf5d9c4ad
rockmsockmjesus <graham.cull@gmail.com>
2026-04-08 17:04:28 +0000

    Bump to 0.1.1 with critical fixes

--- src/markseek/__init__.py
+++ src/markseek/__init__.py
@@ -2,7 +2,7 @@
 markseek -- Semantic search over your markdown vault.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["Index", "Config", "VaultWatcher"]
 
 from markseek.core import Index
