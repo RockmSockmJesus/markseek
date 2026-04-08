b34d010a7d2050243e8123bde1b19653398e9d26
rockmsockmjesus <graham.cull@gmail.com>
2026-04-08 16:29:00 +0000

    Release 0.1.0 to PyPI and GitHub
    - Clean license metadata for PyPI compatibility
    - Added __version__ to package
    - Published to https://pypi.org/project/markseek/0.1.0/
    - Published to https://github.com/RockmSockmJesus/markseek

--- src/markseek/__init__.py
+++ src/markseek/__init__.py
@@ -1,1 +1,10 @@
-# markseek -- semantic search engine
+"""
+markseek -- Semantic search over your markdown vault.
+"""
+
+__version__ = "0.1.0"
+__all__ = ["Index", "Config", "VaultWatcher"]
+
+from markseek.core import Index
+from markseek.config import Config
+from markseek.watcher import VaultWatcher
